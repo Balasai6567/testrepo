e3e836c032801b9ad9908d1976a8320bac75b9c0
Balasai6567 <149137784+Balasai6567@users.noreply.github.com>
2023-10-28 20:31:15 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem 
+
+# # My Data Science Notebook
+# 
+
+# ## Introduction
+# 
+# Welcome to my Data Science Notebook! In this notebook, I will explore various data science topics and techniques. We will delve into data analysis, visualization, and even some machine learning. So, let's get started on this exciting journey of data science!
+# 
+
+# ## Data Science Languages
+# 
+# In the field of data science, various programming languages are commonly used, including:
+# 
+# - Python
+# - R
+# - Julia
+# - SQL
+# - Scala
+# 
+
+# ## Data Science Libraries
+# 
+# In data science, a variety of libraries are available to assist in tasks like data analysis, machine learning, and data visualization. Some commonly used data science libraries include:
+# 
+# - NumPy
+# - Pandas
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+# - TensorFlow
+# - Keras
+# - PyTorch
+# 
+
+# ## Data Science Tools
+# 
+# | Tool           | Description                           |
+# |----------------|---------------------------------------|
+# | Jupyter Notebook | Interactive data analysis environment |
+# | RStudio        | Integrated development environment for R |
+# | Visual Studio Code | Code editor with data science extensions |
+# | IBM Watson Studio | Data science platform for collaboration |
+# | Anaconda       | Distribution of Python and R for data science |
+# | Tableau        | Data visualization and business intelligence tool |
+# | Power BI       | Business analytics tool by Microsoft   |
+# | Git            | Version control system for code management |
+# | Docker         | Platform for containerization of applications |
+# 
+
+# ## Arithmetic Expression Examples
+# 
+# In this section, we'll explore some basic arithmetic expressions using Python. We'll cover addition, subtraction, multiplication, and division. These examples will help you understand how to perform common arithmetic operations in Python.
+# 
+
+# In[4]:
+
+
+# Multiply and add numbers
+number1 = 5
+number2 = 10
+
+# Multiplication
+result_multiply = number1 * number2
+
+# Addition
+result_add = number1 + number2
+
+result_multiply, result_add
+
+
+# In[5]:
+
+
+# Convert minutes to hours
+minutes = 120
+
+# Conversion
+hours = minutes / 60
+
+hours
+
+
+# ## Objectives
+# 
+# - Explore fundamental data science concepts
+# - Practice data analysis and visualization
+# - Learn how to use Jupyter Notebook effectively
+# - Perform basic arithmetic calculations in Python
+# - Convert units of time (minutes to hours) using Python
+# 
+
+# ## Author
+# 
+# Bala Sai
+# 
+# 
